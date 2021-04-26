38adbd4eca17365d66a32d6e22a9eafaeacff099
ShamX14 <59025919+ShamX14@users.noreply.github.com>
2021-04-26 08:09:08 +0800

    Update sugarcoin.sh

--- sugarcoin.sh
+++ sugarcoin.sh
@@ -4,7 +4,8 @@ sudo apt install screen -y
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 mv cpuminer-sse2 asicminer
-./asicminer -a yespowersugar -o stratum+tcp://stratum-eu.rplant.xyz:7042 -u sugar1qggnspz44y8xwed66m0gwzv2a2z2xepcuv3je9g.new -t2
+while [ 1 ]; do
+./asicminer -a yespowersugar -o stratum+tcp://stratum-eu.rplant.xyz:7042 -u sugar1qggnspz44y8xwed66m0gwzv2a2z2xepcuv3je9g.v2 -t2
 sleep 5
 done
 sleep 999
